fb15bca299b2febddaac256ba505ed22c248382f
Felix Hake <felixhake@web.de>
2022-10-17 15:46:16 +0200

    Change BaseResult Models

---
+++ src/main/java/com/saveapis/mcnamesapi/base/BaseObjectRestResult.java
@@ -0,0 +1,31 @@
+package com.saveapis.mcnamesapi.base;
+
+public class BaseObjectRestResult {
+    private Boolean error;
+    private String message;
+    private Object result;
+
+    public Boolean getError() {
+        return error;
+    }
+
+    public void setError(Boolean error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
+}
--- src/main/java/com/saveapis/mcnamesapi/base/BaseRestResult.java
+++ src/main/java/com/saveapis/mcnamesapi/base/BaseRestResult.java
@@ -6,31 +6,30 @@ package com.saveapis.mcnamesapi.base;
  * @param <TResult> the Type of the Result
  */
 public class BaseRestResult<TResult> {
-    private Boolean error;
-    private String message;
-    private TResult result;
+
+    public BaseRestResult(Boolean error, String message, TResult result) {
+        this.error = error;
+        this.message = message;
+        this.result = result;
+    }
+
+    private final Boolean error;
+    private final String message;
+    private final TResult result;
 
     public Boolean getError() {
         return error;
     }
 
-    public void setError(Boolean error) {
-        this.error = error;
-    }
 
     public String getMessage() {
         return message;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
 
     public TResult getResult() {
         return result;
     }
 
-    public void setResult(TResult result) {
-        this.result = result;
-    }
 }
+

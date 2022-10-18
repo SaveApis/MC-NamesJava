4a2e95561dd7ddd556d59eb3fac68ab1c10d157e
Felix Hake <felixhake@web.de>
2022-10-18 16:00:41 +0200

    ================== Test ================== Change output! ==========================================

--- src/test/java/rest/TestTranslationRequest.java
+++ src/test/java/rest/TestTranslationRequest.java
@@ -1,14 +1,18 @@
 package rest;
 
+import com.saveapis.mcnamesapi.api.models.translations.ReadonlyTranslationInfo;
 import com.saveapis.mcnamesapi.api.services.TranslationService;
+import com.saveapis.mcnamesapi.api.utils.ReadonlyCollection;
 import com.saveapis.mcnamesapi.utils.WebUtils;
 
-import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 
 public class TestTranslationRequest {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         WebUtils.setBaseUrl("http://localhost:5002");
-        System.out.println(Objects.requireNonNull(TranslationService.All().get().get(0)).displayName());
+        ReadonlyCollection<ReadonlyTranslationInfo> infos = TranslationService.All().get();
+        for (ReadonlyTranslationInfo info : infos.toList()) {
+            System.out.println(info.displayName());
+        }
     }
 }

8685a01b4a1bf4a6a8df550d90fa397e152a68e3
SaveFeelix <felixhake@web.de>
2022-10-19 07:22:19 +0200

    ==================== Tests ==================== Update Test-Classes ===============================================

--- src/test/java/TestWebRequest.java
+++ src/test/java/TestWebRequest.java
@@ -1,3 +1,4 @@
+import com.saveapis.mcnamesapi.models.base.BaseObjectRestResult;
 import com.saveapis.mcnamesapi.types.RestPaths;
 import com.saveapis.mcnamesapi.utils.JsonUtils;
 import com.saveapis.mcnamesapi.utils.WebUtils;
@@ -7,7 +8,11 @@ public class TestWebRequest {
         // Method to change the BaseURL to access the REST-API
         // WebUtils.setBaseUrl("yourPathToTheBaseUrlOfTheRestApi");
 
-        Object object = WebUtils.get(RestPaths.TRANSLATION, Object.class);
-        System.out.println(JsonUtils.toJson(object));
+        //Object object = WebUtils.get(RestPaths.TRANSLATION, Object.class);
+        //System.out.println(JsonUtils.toJson(object));
+
+        WebUtils.setBaseUrl("http://localhost:5002");
+        BaseObjectRestResult result = WebUtils.get(RestPaths.TRANSLATION, BaseObjectRestResult.class);
+        System.out.println(JsonUtils.toJson(result));
     }
 }
--- src/test/java/rest/TestTranslationRequest.java
+++ src/test/java/rest/TestTranslationRequest.java
@@ -1,18 +1,14 @@
 package rest;
 
-import com.saveapis.mcnamesapi.api.models.translations.ReadonlyTranslationInfo;
-import com.saveapis.mcnamesapi.api.services.TranslationService;
-import com.saveapis.mcnamesapi.api.utils.ReadonlyCollection;
-import com.saveapis.mcnamesapi.utils.WebUtils;
-
-import java.util.concurrent.ExecutionException;
-
 public class TestTranslationRequest {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) {
+        /*
         WebUtils.setBaseUrl("http://localhost:5002");
         ReadonlyCollection<ReadonlyTranslationInfo> infos = TranslationService.All().get();
         for (ReadonlyTranslationInfo info : infos.toList()) {
             System.out.println(info.displayName());
         }
+
+         */
     }
 }

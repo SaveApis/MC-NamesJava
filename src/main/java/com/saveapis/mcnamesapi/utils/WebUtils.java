cb629f2f1d5d9019283ae2e09683a1fd206bbb12
SaveFeelix <felixhake@web.de>
2022-10-19 07:21:29 +0200

    ==================== Utils ==================== Recode to Spring-Framework ===============================================

--- src/main/java/com/saveapis/mcnamesapi/utils/WebUtils.java
+++ src/main/java/com/saveapis/mcnamesapi/utils/WebUtils.java
@@ -3,23 +3,20 @@ package com.saveapis.mcnamesapi.utils;
 import com.saveapis.mcnamesapi.types.RestPaths;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.restlet.Context;
-import org.restlet.data.MediaType;
-import org.restlet.resource.ClientResource;
-
-import java.io.IOException;
-import java.util.logging.Level;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 public class WebUtils {
+
+    private static final RestTemplate client = new RestTemplate();
+
     public static <T> @Nullable T get(@NotNull String url, @NotNull Class<T> clazz) {
-        try {
-            ClientResource clientResource = createClient(url);
-            String requestResult = clientResource.get(MediaType.APPLICATION_JSON).getText();
-            clientResource.release();
-            return JsonUtils.toObject(requestResult, clazz);
-        } catch (IOException e) {
+        ResponseEntity<T> response = client.getForEntity(url, clazz);
+        if (response.getStatusCode() != HttpStatus.OK)
             return null;
-        }
+
+        return response.getBody();
     }
 
     public static <T> @Nullable T get(@NotNull RestPaths type, @Nullable String path, @NotNull Class<T> clazz) {
@@ -31,6 +28,7 @@ public class WebUtils {
         return get(type, null, clazz);
     }
 
+    /*
     public static <T> @Nullable T post(@NotNull RestPaths type, @Nullable String path, @NotNull Class<T> clazz) {
         try {
             String url = getBaseUrl() + "/" + type.getPath() + "/" + (path != null ? path + "/" : "");
@@ -42,7 +40,7 @@ public class WebUtils {
             return null;
         }
     }
-
+    */
 
     public static String getBaseUrl() {
         return System.getProperty("MC-NamesBaseUrl", "https://api.saveapis.com/mc-namesapi");
@@ -50,13 +48,5 @@ public class WebUtils {
 
     public static void setBaseUrl(String baseUrl) {
         System.setProperty("MC-NamesBaseUrl", baseUrl);
-    }
-
-    private static @NotNull ClientResource createClient(@NotNull String url) {
-
-        // Disable Logging Message
-        Context context = new Context();
-        context.getLogger().setLevel(Level.WARNING);
-        return new ClientResource(context, url);
     }
 }

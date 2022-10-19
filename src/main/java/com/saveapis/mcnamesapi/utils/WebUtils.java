6e069c078d710f8a25e5c489d36c3f4e8216723f
SaveFeelix <felixhake@web.de>
2022-10-19 15:51:52 +0200

    ================== Utils ================== Change url-generation > WebUtils#get Create post-methods > WebUtils.java ===========================================

--- src/main/java/com/saveapis/mcnamesapi/utils/WebUtils.java
+++ src/main/java/com/saveapis/mcnamesapi/utils/WebUtils.java
@@ -3,6 +3,7 @@ package com.saveapis.mcnamesapi.utils;
 import com.saveapis.mcnamesapi.types.RestPaths;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -20,7 +21,7 @@ public class WebUtils {
     }
 
     public static <T> @Nullable T get(@NotNull RestPaths type, @Nullable String path, @NotNull Class<T> clazz) {
-        String url = getBaseUrl() + "/" + type.getPath() + "/" + (path != null ? path + "/" : "");
+        String url = getBaseUrl() + "/" + type.getPath() + "/" + path;
         return get(url, clazz);
     }
 
@@ -28,19 +29,22 @@ public class WebUtils {
         return get(type, null, clazz);
     }
 
-    /*
-    public static <T> @Nullable T post(@NotNull RestPaths type, @Nullable String path, @NotNull Class<T> clazz) {
-        try {
-            String url = getBaseUrl() + "/" + type.getPath() + "/" + (path != null ? path + "/" : "");
-            ClientResource clientResource = createClient(url);
-            String postResult = clientResource.post(null).getText();
-            clientResource.release();
-            return JsonUtils.toObject(postResult, clazz);
-        } catch (IOException e) {
+    public static <T> @Nullable T post(@NotNull String url, @NotNull Class<T> clazz) {
+        // TODO Fix 405 Error
+        ResponseEntity<T> response = client.postForEntity(url, HttpEntity.EMPTY, clazz);
+        if (response.getStatusCode() != HttpStatus.OK)
             return null;
-        }
+        return response.getBody();
     }
-    */
+
+    public static <T> @Nullable T post(@NotNull RestPaths type, @Nullable String path, @NotNull Class<T> clazz) {
+        String url = getBaseUrl() + "/" + type.getPath() + "/" + path;
+        return post(url, clazz);
+    }
+
+    public static <T> @Nullable T post(@NotNull RestPaths type, @NotNull Class<T> clazz) {
+        return post(type, null, clazz);
+    }
 
     public static String getBaseUrl() {
         return System.getProperty("MC-NamesBaseUrl", "https://api.saveapis.com/mc-namesapi");
